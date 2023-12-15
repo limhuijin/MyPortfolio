fa7f73a0f55e91ab08b065859bbe148ec0f18b06
limhuijin <gabriel227@naver.com>
2023-12-15 13:37:20 +0900

    변경

--- gallery.js
+++ gallery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    for(let i = 1; i < 13; i++){
+    for(let i = 1; i <= 12; i++){
         let imgSrc = '/MyPortfolio/photo/galleryImg' + i + '.jpg';
         let imgEle = $('<img>').attr('src', imgSrc);
 
