f72b27fd8ee91dd8f650b572e411cb26debb767b
limhuijin <gabriel227@naver.com>
2023-12-15 13:14:20 +0900

    변경

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     for(let i = 1; i < 13; i++){
-        let imgSrc = 'galleryImg' + i + '.jpg';
+        let imgSrc = '/MyPortfolio/galleryImg' + i + '.jpg';
         let imgEle = $('<img>').attr('src', imgSrc);
 
         $('<div></div>').addClass('box').append(imgEle).appendTo('.contents');
