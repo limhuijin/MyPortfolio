6547afd5f94de52163f8711c21ac06bffb6a5b07
limhuijin <gabriel227@naver.com>
2023-12-15 12:47:29 +0900

    변경

--- gallery.js
+++ gallery.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
-    for(let i = 0; i < 100; i++){
-        $('<div></div>').addClass('box').css({
-            width: 100,
-            height: Math.floor(Math.random() * 100) + 50,
-        }).appendTo('#contents');
-    }
-
     $.getScript('https://cdn.jsdelivr.net/jquery.masonry/4.2.2/masonry.pkgd.min.js', function(){
+        for(let i = 0; i < 100; i++){
+            $('<div></div>').addClass('box').css({
+                width: 100,
+                height: Math.floor(Math.random() * 100) + 50,
+            }).appendTo('#contents');
+        }
+        
         $('#contents').masonry({
             columWidth:110  
         });
+        
     });
 });
