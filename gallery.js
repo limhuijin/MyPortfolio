2dc97de86cd10e57977b987d5034a28ce9f15fae
limhuijin <gabriel227@naver.com>
2023-12-15 12:16:13 +0900

    갤러리 추가

---
+++ gallery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    for(let i = 0; i < 100; i++){
+        $('<div><div>').addClass('box').css({
+            width: 100,
+            height: Math.floor(Math.random() * 100) + 50,
+        }).appendTo('body');
+    }
+
+    $('body').masonry({
+        columWidth:110
+    });
+});
